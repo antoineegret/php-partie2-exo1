be938f12da048e0c84a47e06d581e40431a4e6b2
antoineegret <a.egret@laposte.net>
2019-12-09 12:02:33 +0100

    exo1 partie2 php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $age = 27;
+    if ($age < 18) {
+    echo "vous ête mineur";
+    } 
+    if ($age > 18) {
+    echo "vous ête majeur";    
+    }
+?>
+
+</body>
+</html>
